782b52677f8ad7d4ad7544590e23e6de48dc7fa7
bayudwip <bayu.prazetyo@gmail.com>
2017-04-09 12:35:46 +0700

    Add files via upload

---
+++ ALgoritma dan Pemrograman 2/P2K/BayuDwiPrastyo_2016470126_Modul2.c
@@ -0,0 +1,53 @@
+#include<stdio.h>
+
+int i, j, data, k, bantu, h[100], A[100];
+main()
+{
+printf ("=====Bayu Dwi Prastyo=====\n");
+printf ("========2016470126========");
+printf ("\n\n");
+printf ("INPUT JUMLAH DATA = "); scanf ("%d", &data);
+printf ("\n\n");
+for (i=1; i <= data; i++)
+{
+printf ("A[%d] = ", i); scanf ("%d", &h[i]);}
+printf ("\n\n");
+printf("****************** DATA BELUM TERURUT ******************");
+printf("\n\n");
+printf(" A = { "); 
+for (i=1; i <= data; i++)
+{
+	printf ("%d,", h[i]);
+}
+printf (" } ");
+for(i=1; i<= data; i++)
+{
+	for (j = data; j >= (i+1); j--)
+	{
+		if (h[j]>h[j-1])
+		{ 
+		bantu=h[j-1];
+		h[j-1]=h[j];
+		h[j]=bantu;
+		printf("\n\n");
+		
+		printf(" A = { ");
+		for(k=1; k <= data; k++)
+		{
+			printf("%d,",h[k]);
+		}
+		printf(" }\a ");
+		}
+	}
+}
+printf("\n\n");
+printf("********************* DATA SUDAH TERURUT ******************** ");
+printf("\n");
+printf(" A = { ");
+for (i=1; i <= data ;i++)
+{
+printf("%d,",h[i]);
+}
+printf ("}");
+return 0;
+}
