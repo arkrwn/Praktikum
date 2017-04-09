3f506e0a66b76f4f33d47a4f51047ef58e173988
bayu178 <bayusamoedra_2@yahoo.com>
2017-04-09 12:06:15 +0700

    Add files via upload

---
+++ ALgoritma dan Pemrograman 2/P2K/bayu samudra_2016470127_modul2.c
@@ -0,0 +1,56 @@
+#include<stdio.h>
+#include<conio.h>
+
+int i , j , data, k , bantu, h[100], A[100];
+
+int main()
+
+{
+	printf("masukkan jumlah data = "); scanf("%d",&data);
+	printf("\n\n");
+	
+	for (i=1; i <= data; i++)
+	{
+		printf("A[%d] = ", i); scanf("%d", &h[i]);
+	}
+	printf("\n\n");
+	printf("===============data tidak urut===============");
+	printf("\n\n");
+	printf("A = { ");
+	
+	for (i = 1; i <=data; i++)
+	{
+		printf("%d,", h[i]);
+	}
+	printf(" } ");
+	for (i=1; i <= (data-1); i++)
+	{
+		for (j=data; j >= (i+1); j--)
+		{
+			if (h[j] > h[j-1])
+			{
+				bantu = h[j-1];
+				h[j-1] = h[j];
+				h[j] = bantu;
+			    printf("\n\n");
+			    printf("A = { ");
+			    for (k=1; k <= data; k++)
+			    {
+			    	printf("%d,", h[k]);
+				}
+				printf(" } \a");
+			}	
+		}
+	}
+printf("\n\n");
+printf("===============data urut===============");
+printf("\n\n");
+printf (" A= { ");
+for (i=1; i <= data; i++)
+{
+	printf("%d,", h[i]);
+}
+printf(" } ");
+getch();
+return 0;
+}
